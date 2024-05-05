e5c885d7bad14eeb8d423cbfad57efe1c0a66900
Dasuni Geeganage <dasunin30@gmail.com>
2024-05-05 07:52:28 +0530

    Update the repo structure

--- petcare-enterprise-apps/b2c/web-app/react/public/config.js
+++ petcare-enterprise-apps/b2c/web-app/react/public/config.js
@@ -1,12 +1,12 @@
 window.config = {
-  baseUrl: "",
-  clientID: "",
+  baseUrl: "https://api.asgardeo.io/t/dasuniorgeu",
+  clientID: "7yzGkTH8uFCPO8hc8Yg2ris1TB8a",
   signInRedirectURL: "http://localhost:3000",
   signOutRedirectURL: "http://localhost:3000",
-  myAccountAppURL: "",
+  myAccountAppURL: "https://myaccount.asgardeo.io/t/dasuniorgeu",
   petManagementServiceURL: "http://localhost:9090",
   billingServerURL: "http://localhost:9091",
   salesforceServerURL: "http://localhost:9092",
-  scope: ["openid", "email", "profile", "acr"],
+  scope: ["openid", "email", "profile"],
   enableOIDCSessionManagement: true
 };
