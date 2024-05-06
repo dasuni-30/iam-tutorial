6d8c49890fa850b97fab74ae1b169ad301b94bb7
Dasuni Geeganage <dasunin30@gmail.com>
2024-05-06 14:40:14 +0530

    Remove the petcare mobile app

--- petcare-enterprise-apps/b2c/mobile-app/petcare/app/src/main/java/com/wso2_sample/api_auth_sample/model/ui/activities/login/fragments/auth/auth_method/passkey/authenticator/metadata/PasskeyAdditionalData.kt
+++
@@ -1,5 +0,0 @@
-package com.wso2_sample.api_auth_sample.model.ui.activities.login.fragments.auth.auth_method.passkey.authenticator.metadata
-
-data class PasskeyAdditionalData(
-    val challengeData: String
-)
